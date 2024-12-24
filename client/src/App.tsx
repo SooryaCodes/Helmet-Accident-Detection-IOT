abb1a3cf6c10ed3670102bcb728d05070fc99416
sooryakriz11 <sooryakriz111@gmail.com>
2024-12-24 15:54:13 +0530

    route udpate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 import Signup from "./pages/Signup";
 import EmergencyContacts from "./pages/EmergencyContacts";
@@ -15,62 +20,18 @@ const App: React.FC = () => {
     <UserProvider>
       <Router>
         <Routes>
-          {/* Routes without Layout */}
-          <Route
-            path="/signup"
-            element={
-              <Layout>
-                <Signup />
-              </Layout>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <Layout>
-                <Login />
-              </Layout>
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <Layout>
-                <Dashboard />
-              </Layout>
-            }
-          />
-          {/* Protected Routes with Layout */}
-          <Route
-            path="/emergency-contacts"
-            element={
-              <Layout>
-                <EmergencyContacts />
-              </Layout>
-            }
-          />
-          <Route
-            path="/link-device"
-            element={
-              <Layout>
-                <DeviceRegistration />
-              </Layout>
-            }
-          />
-          <Route
-            path="/"
-            element={
-                <Home />
-            }
-          />
-          <Route
-            path="/bt"
-            element={
-              <Layout>
-                <BluetoothDeviceScanner />
-              </Layout>
-            }
-          />
+          {/* Routes */}
+          {/* Uncommented as needed */}
+          {/* <Route path="/signup" element={<Layout><Signup /></Layout>} />
+        <Route path="/login" element={<Layout><Login /></Layout>} />
+        <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+        <Route path="/emergency-contacts" element={<Layout><EmergencyContacts /></Layout>} />
+        <Route path="/link-device" element={<Layout><DeviceRegistration /></Layout>} /> */}
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/bt" element={<Layout><BluetoothDeviceScanner /></Layout>} /> */}
+
+          {/* Catch-all redirect */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
     </UserProvider>
